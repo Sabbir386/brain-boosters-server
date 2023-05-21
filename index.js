33faeea539f89b539b359f24c0adeeafc1a7cd74
Sabbir386 <sabbir386.sa@gmail.com>
2023-05-21 19:30:51 +0600

    vercel deploy successfully

--- index.js
+++ index.js
@@ -27,13 +27,12 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect();
         const toyCollection = client.db('toyData').collection('toy');
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
 
 
 
